96ec562e8bb0023436e5ba77042529f38ebeea11
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-05-26 16:43:14 +0300

    Add Header component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
+import Form from "./components/Form";
+import Header from "./components/header/Header";
 import "./App.css";
-
-const Form = () => {
-  return (
-    <form>
-      <label htmlFor='email'>Email</label>
-      <input type='email' id='email' placeholder='Enter your email' />
-    </form>
-  );
-};
 
 const App = () => {
   return (
     <>
+      <Header />
       <Form />
     </>
   );
