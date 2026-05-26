762c33361369488b24cab4af96c3cebf2b42e80a
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-05-26 16:25:17 +0300

    Add basic email input form with submit button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
 
 const App = () => {
-  return <h2>Hello React</h2>;
-}
+  return (
+    <>
+      <input type='email' placeholder='Enter your email' />
+      <button>Submit</button>
+    </>
+  );
+};
 
-export default App
+export default App;
