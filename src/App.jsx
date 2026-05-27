56751f3f735b0318daa8d70cb69633028e2b6d7a
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-05-27 13:37:08 +0300

    Add Products component to display product list with sample data

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import Form from "./components/Form";
 import Header from "./components/header/Header";
 import "./App.css";
+import Products from "./components/products/Products";
 
 const App = () => {
-  const message = "Hello React";
-  console.log("From console");
+  const products = [
+    { title: "Pizza", desc: "this is a pizza", price: 120, rate: 4 },
+    { title: "Laptop", desc: "this is a laptop", price: 500, rate: 3 },
+    { title: "Mobile", desc: "this is a mobile", price: 350, rate: 5 },
+  ];
   return (
     <>
-      <h1>{message}</h1>
       <Header />
       <Form />
+      <Products products={products} />
     </>
   );
 };
