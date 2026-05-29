f14db3ea57ae6aa0ab3689f0e306b1074edc7ade
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-05-29 10:31:23 +0300

    Add buttons to dynamically change first name and last name

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,12 @@ const App = () => {
       <h1>
         Hello, {firstName} {lastName}
       </h1>
+      <button type='button' onClick={() => setFirstName("Bilal")}>
+        Change FirstName
+      </button>
+      <button type='button' onClick={() => setLastName("Abu Draz")}>
+        Change LastName
+      </button>
     </>
   );
 };
