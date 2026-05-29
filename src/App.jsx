7e28b459ed4257069f7ddcfeb7f5f768d2afcf54
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-05-29 17:17:59 +0300

    Add API call to fetch photos and log response to console

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,12 @@
 import './App.css';
-import { useState } from 'react';
 
 const App = () => {
-  const [userName, setUserName] = useState('');
-  const [password, setPassword] = useState('');
+  fetch('https://jsonplaceholder.typicode.com/photos')
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (userName === '' || password === '') alert('Please fill in all fields!');
-    console.log({ username: userName, password: password });
-  };
-
-  return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor='username'>Username</label>
-        <input
-          type='text'
-          name='username'
-          id='username'
-          value={userName}
-          onChange={(e) => setUserName(e.target.value)}
-        />
-        <label htmlFor='password'>Password</label>
-        <input
-          type='password'
-          name='password'
-          id='password'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type='submit'>Login</button>
-      </form>
-    </>
-  );
+  return <></>;
 };
 
 export default App;
