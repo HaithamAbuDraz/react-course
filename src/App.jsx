21577fe61b0b0a1eb97943703245db0826645f83
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-05-29 10:27:36 +0300

    Add firstName and lastName state with greeting display

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,13 @@ import "./App.css";
 import { useState } from "react";
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  const [firstName, setFirstName] = useState("Haitham");
+  const [lastName, setLastName] = useState("Mohammed");
   return (
     <>
-      <h1>Hello, World!</h1>
-      <div>Count: {count}</div>
-      <button type='button' onClick={() => setCount((prev) => prev + 1)}>
-        Increment
-      </button>
-      <button type='button' onClick={() => setCount((prev) => prev - 1)}>
-        Decrement
-      </button>
-      <button type='button' onClick={() => setCount(0)}>
-        Reset
-      </button>
+      <h1>
+        Hello, {firstName} {lastName}
+      </h1>
     </>
   );
 };
