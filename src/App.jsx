3c5128740d358109d32be818bd50fafd068bea46
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-05-29 17:19:04 +0300

    Improve fetch implementation with async/await

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import './App.css';
 
 const App = () => {
-  fetch('https://jsonplaceholder.typicode.com/photos')
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err));
+  // fetch('https://jsonplaceholder.typicode.com/photos')
+  //   .then((res) => res.json())
+  //   .then((data) => console.log(data))
+  //   .catch((err) => console.log(err));
 
+  const getData = async () => {
+    try {
+      const res = await fetch('https://jsonplaceholder.typicode.com/photos');
+      const data = await res.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  getData();
   return <></>;
 };
 
