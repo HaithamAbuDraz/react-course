0a9d433fc7bde3a2d929dbab4098ea82d2d250a1
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-05-30 18:13:37 +0300

    Replace Middle component with Top component while maintaining prop flow

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css';
-import Middle from './components/Middle';
+import Top from './components/Top';
 
 const App = () => {
   const username = 'Haitham';
   return (
     <>
-      <h1>Hello, Reacts</h1>
-      <Middle username={username} />
+      <h1>Hello, React</h1>
+      <Top username={username} />
     </>
   );
 };
