8610a958a64135a1b3acf37bd7512221b5845d39
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-05-30 12:23:15 +0300

    Implement document click event listener in useEffect with cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,20 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import './App.css';
 
 const App = () => {
-  const [fetchedData, setFetchedData] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
-
+  const listener = () => {
+    console.log('Hello');
+  };
   useEffect(() => {
-    const getData = async () => {
-      setIsLoading(true);
-      try {
-        setIsLoading(true);
-        const res = await fetch('https://jsonplaceholder.typicode.com/photos');
-        const data = await res.json();
-        if (res.status === 200) {
-          setFetchedData(data);
-          setIsLoading(false);
-        } else {
-          setIsLoading(false);
-          setErrorMessage('Something went wrong');
-        }
-        console.log(data);
-      } catch (err) {
-        setErrorMessage(JSON.stringify(err));
-        setIsLoading(false);
-        console.log(err);
-      }
+    document.addEventListener('click', listener);
+    return () => {
+      document.removeEventListener('click', listener);
     };
-    getData();
-  }, []);
+  });
 
   return (
     <>
-      {isLoading && <h1>Loading...</h1>}
-      {!isLoading && errorMessage && <h1>{errorMessage}</h1>}
-      {isLoading === false && fetchedData && (
-        <ul>
-          {fetchedData.slice(0, 9).map((item) => (
-            <li key={item.id}>
-              <h2>{item.title}</h2>
-              <img
-                src={item.url}
-                alt={item.title}
-                width='100px'
-                height='100px'
-              />
-            </li>
-          ))}
-        </ul>
-      )}
+      <h1>Hello, Reacts</h1>
     </>
   );
 };
