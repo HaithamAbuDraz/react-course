42618e1728cb8b24a5ba4613cb2c7054a5cdf435
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-06-01 16:16:19 +0300

    Use useContext to access cartItems from global context

--- src/components/CartItems.jsx
+++ src/components/CartItems.jsx
@@ -1,12 +1,15 @@
+import { useContext } from 'react';
 import CartItem from './CartItem';
+import { ShoppingCartContext } from '../context/ShoppingCartContext';
 
-const CartItems = ({ cartItems }) => {
+const CartItems = () => {
+  const { cartItems } = useContext(ShoppingCartContext);
   return (
     <div>
       <h1>Cart {cartItems.length}</h1>
       <ul>
         {cartItems.map((item) => (
-          <CartItem key={item.id} cartItem={item}/>
+          <CartItem key={item.id} cartItem={item} />
         ))}
       </ul>
     </div>
