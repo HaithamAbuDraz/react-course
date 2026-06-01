59a0c1f895ea20349ceb129af00224041dce5762
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-06-01 16:18:21 +0300

    Provide shopping cart context to entire application

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { Toaster } from 'react-hot-toast';
-import './index.css'
-import App from './App.jsx'
+import './index.css';
+import App from './App.jsx';
+import ShoppingCartContextProvider from './context/ShoppingCartContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Toaster />
-    <App />
+    <ShoppingCartContextProvider>
+      <App />
+    </ShoppingCartContextProvider>
   </StrictMode>,
-)
+);
