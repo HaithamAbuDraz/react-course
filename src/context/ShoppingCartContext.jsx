33d58de4ae523dce6e6f6e6ce3ce18359cd1f5b3
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-06-01 16:12:30 +0300

    Create ShoppingCartContext for global cart state management

---
+++ src/context/ShoppingCartContext.jsx
@@ -0,0 +1,23 @@
+import { createContext, useEffect, useState } from 'react';
+
+export const ShoppingCartContext = createContext({});
+
+const initialCartItems = localStorage.getItem('cartItems')
+  ? JSON.parse(localStorage.getItem('cartItems'))
+  : [];
+
+const ShoppingCartContextProvider = ({ children }) => {
+  const [cartItems, setCartItems] = useState(initialCartItems);
+
+  useEffect(() => {
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+  }, [cartItems]);
+
+  return (
+    <ShoppingCartContext.Provider value={{ cartItems, setCartItems }}>
+      {children}
+    </ShoppingCartContext.Provider>
+  );
+};
+
+export default ShoppingCartContextProvider;
