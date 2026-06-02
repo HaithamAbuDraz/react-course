456f986a4d92966c3c0c4cdc11b3d54f5c16d65b
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-06-02 11:31:11 +0300

    Implement counter with useReducer hook for state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,30 @@
-import { useEffect, useState } from 'react';
+import { useReducer } from 'react';
 import './App.css';
-import Products from './components/Products';
-import CartItems from './components/CartItems';
 
-const dummyData = [
-  {
-    id: 1,
-    title:
-      'HP OMEN 45L Gaming 5090 Desktop PC, Intel Core Ultra 9 285K, GeForce RTX 5090, 128GB DDR5, 4TB SSD, Support 4-Display 4K, Light Studio, HDMI, DP, Wi-Fi 6E, Windows 11 Pro, AI-Ready Workstation Desktop',
-    img: 'https://m.media-amazon.com/images/I/517L5CH1lXL._AC_UY436_FMwebp_QL65_.jpg',
-    rate: 3,
-    price: 19891.86,
-    insteadOf: 20145.86,
-  },
-  {
-    id: 2,
-    title:
-      'HP Omen MAX 45L 5090 Gaming Desktop, AMD Ryzen 9 9900X3D 5.5GHz, Geforce RTX 5090, 64GB DDR5 RAM, 4TB SSD, Liquid Cooler, RGB Light Studio, 3X DP, Support 3-Monitor 4K, Windows 11 Pro, Black',
-    img: 'https://m.media-amazon.com/images/I/61QYqlq7pJL._AC_UY436_FMwebp_QL65_.jpg',
-    rate: 5,
-    price: 17049.76,
-    insteadOf: 18015.76,
-  },
-  {
-    id: 3,
-    title:
-      'PCONLINE HP Omen MAX 45L 5090 Gaming Desktop, AMD Ryzen 9 9900X3D 5.5GHz, Geforce RTX 5090, 128GB DDR5 RAM, 8TB SSD, RGB Light Studio, LCD Liquid Cooler, DP, HDMI, Wi-Fi 7, Windows 11 Pro, Black',
-    img: 'https://m.media-amazon.com/images/I/61QYqlq7pJL._AC_UY436_FMwebp_QL65_.jpg',
-    rate: 4,
-    price: 20744.49,
-    insteadOf: 21745.86,
-  },
-];
+const reducer = (state, action) => {
+  if (action === 'increment') {
+    return state + 1;
+  } else if (action === 'decrement') {
+    return state - 1;
+  } else if (action === 'reset') {
+    return 0;
+  }
+};
 
 const App = () => {
-  const [products, setProducts] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    const getData = async () => {
-      setIsLoading(true);
-      await new Promise((resolve) =>
-        setTimeout(resolve, Math.random() * 6000 + 500),
-      );
-      setProducts(dummyData);
-      setIsLoading(false);
-    };
-    getData();
-  }, []);
-
+  const [count, dispatch] = useReducer(reducer, 0);
   return (
     <>
-      <h1>Hello React</h1>
-      {isLoading && <p>Loading...</p>}
-      {!isLoading && products && (
-        <Products products={products} />
-      )}
-      <CartItems />
+      <h1>Count: {count}</h1>
+      <button type='button' onClick={() => dispatch('increment')}>
+        Increment
+      </button>
+      <button type='button' onClick={() => dispatch('decrement')}>
+        Decrement
+      </button>
+      <button type='button' onClick={() => dispatch('reset')}>
+        Reset
+      </button>
     </>
   );
 };
