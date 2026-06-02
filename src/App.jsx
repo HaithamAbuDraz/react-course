5e5e84e36e0aef1069cd9de0833426bc5603df89
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-06-02 13:33:57 +0300

    Implement two independent counters with reusable reducer and payload

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,79 @@
 import { useReducer } from 'react';
 import './App.css';
 
-const initialState = 0;
+const initialCountOneState = 0;
+const initialCountTwoState = 2;
 
 const reducer = (state, action) => {
-  switch (action) {
+  switch (action.type) {
     case 'increment':
-      return state + 1;
+      return state + action.payload;
     case 'decrement':
-      return state - 1;
+      return state - action.payload;
     case 'reset':
-      return initialState;
+      return action.payload;
     default:
       return state;
   }
 };
 
 const App = () => {
-  const [count, dispatch] = useReducer(reducer, initialState);
+  const [countOne, dispatchCountOne] = useReducer(
+    reducer,
+    initialCountOneState,
+  );
+  const [countTwo, dispatchCountTwo] = useReducer(
+    reducer,
+    initialCountTwoState,
+  );
   return (
     <>
-      <h1>Count: {count}</h1>
-      <button type='button' onClick={() => dispatch('increment')}>
-        Increment
-      </button>
-      <button type='button' onClick={() => dispatch('decrement')}>
-        Decrement
-      </button>
-      <button type='button' onClick={() => dispatch('reset')}>
-        Reset
-      </button>
+      <div>
+        <h1>Count One: {countOne}</h1>
+        <button
+          type='button'
+          onClick={() => dispatchCountOne({ type: 'increment', payload: 1 })}
+        >
+          Increment
+        </button>
+        <button
+          type='button'
+          onClick={() => dispatchCountOne({ type: 'decrement', payload: 1 })}
+        >
+          Decrement
+        </button>
+        <button
+          type='button'
+          onClick={() =>
+            dispatchCountOne({ type: 'reset', payload: initialCountOneState })
+          }
+        >
+          Reset
+        </button>
+      </div>
+      <div>
+        <h1>Count Two: {countTwo}</h1>
+        <button
+          type='button'
+          onClick={() => dispatchCountTwo({ type: 'increment', payload: 1 })}
+        >
+          Increment
+        </button>
+        <button
+          type='button'
+          onClick={() => dispatchCountTwo({ type: 'decrement', payload: 1 })}
+        >
+          Decrement
+        </button>
+        <button
+          type='button'
+          onClick={() =>
+            dispatchCountTwo({ type: 'reset', payload: initialCountTwoState })
+          }
+        >
+          Reset
+        </button>
+      </div>
     </>
   );
 };
