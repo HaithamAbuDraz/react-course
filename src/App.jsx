dcad81db8ffb86905aaf0ccfcf5991628749fdd3
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-06-03 16:00:53 +0300

    Render ComponentA, B, and C in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,15 @@
-import { useEffect, useRef, useState } from 'react';
+import './App.css';
+import ComponentA from './components/ComponentA';
+import ComponentB from './components/ComponentB';
+import ComponentC from './components/ComponentC';
 
 const App = () => {
-  const intervalRef = useRef(null);
-  const inputRef = useRef(null);
-
-  const [timer, setTimer] = useState(0);
-
-  useEffect(() => {
-    intervalRef.current = setInterval(() => {
-      setTimer((prev) => prev + 1);
-    }, 1000);
-
-    return () => clearInterval(intervalRef.current);
-  }, []);
-
-  useEffect(() => {
-    inputRef.current.focus();
-  }, []);
-
   return (
     <>
       <div>
-        <h1>Timer: {timer}</h1>
-        <button
-          type='button'
-          onClick={() => clearInterval(intervalRef.current)}
-        >
-          Stop Timer
-        </button>
-        <input ref={inputRef} type='email' placeholder='Enter your email' />
+        <ComponentA />
+        <ComponentB />
+        <ComponentC />
       </div>
     </>
   );
