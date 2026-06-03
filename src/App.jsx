61dd802f899c930b2649e0f3b8efa5a2e0a78fb9
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-06-03 15:14:00 +0300

    Implement timer using useRef and setInterval

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,27 @@
-import { useEffect, useReducer } from 'react';
-import axios from 'axios';
-
-const initialState = {
-  loading: false,
-  posts: [],
-  error: '',
-};
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'FETCH_PENDING':
-      return {
-        loading: true,
-        posts: [],
-        error: '',
-      };
-    case 'FETCH_SUCCESS':
-      return {
-        loading: false,
-        posts: action.payload,
-        error: '',
-      };
-    case 'FETCH_ERROR':
-      return {
-        loading: false,
-        posts: [],
-        error: JSON.stringify(action.payload) ?? 'Something went wrong',
-      };
-    default:
-      return state;
-  }
-};
+import { useEffect, useRef, useState } from 'react';
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const intervalRef = useRef(null);
+  const [timer, setTimer] = useState(0);
 
   useEffect(() => {
-    const getPosts = async () => {
-      try {
-        dispatch({ type: 'FETCH_PENDING' });
-        const res = await axios.get(
-          'https://jsonplaceholder.typicode.com/posts',
-        );
-        if (res.status === 200) {
-          dispatch({ type: 'FETCH_SUCCESS', payload: res.data });
-        } else {
-          dispatch({ type: 'FETCH_ERROR' });
-        }
-        console.log(res.data);
-      } catch (error) {
-        dispatch({ type: 'FETCH_ERROR', payload: error });
-        console.error(error);
-      }
-    };
-    getPosts();
+    intervalRef.current = setInterval(() => {
+      setTimer((prev) => prev + 1);
+    }, 1000);
+
+    return () => clearInterval(intervalRef.current);
   }, []);
+
   return (
     <>
       <div>
-        {state.loading && <p>Loading...</p>}
-        {!state.loading && state.error && <p>Error: {state.error}</p>}
-        {!state.loading && state.posts && (
-          <ul>
-            {state.posts.map((post) => (
-              <li key={post.id}>{post.title}</li>
-            ))}
-          </ul>
-        )}
+        <h1>Timer: {timer}</h1>
+        <button
+          type='button'
+          onClick={() => clearInterval(intervalRef.current)}
+        >
+          Stop Timer
+        </button>
       </div>
     </>
   );
