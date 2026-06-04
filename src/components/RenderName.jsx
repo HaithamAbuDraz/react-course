517960f20d119f7cc81971af18e53acd05505445
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-06-04 14:33:16 +0300

    Wrap RenderName with React.memo to prevent unnecessary re-renders

--- src/components/RenderName.jsx
+++ src/components/RenderName.jsx
@@ -1,6 +1,6 @@
-const RenderName = ({ name }) => {
+import { memo } from 'react';
+
+export default memo(function RenderName({ name }) {
   console.log('Rendering RenderName Component');
   return <h2>Name: {name}</h2>;
-};
-
-export default RenderName;
+});
