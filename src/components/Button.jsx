406bab3babd17d0b0ca0ba4dc9fd09356c95de31
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-06-04 16:22:26 +0300

    Wrap Button component with React.memo to prevent unnecessary re-renders

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,9 @@
-const Button = ({ children, onClick }) => {
+import { memo } from 'react';
+
+const Button = memo(({ children, onClick }) => {
   console.log('Rendering Button:', children);
+
   return <button onClick={onClick}>{children}</button>;
-};
+});
 
 export default Button;
