5a58671e90434fbf7da6a8a4abecc38e19197c5b
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-06-05 16:57:00 +0300

    Render TodoList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,10 @@
-import { useCallback, useState } from 'react';
-import './App.css';
-import Count from './components/Count';
-import Button from './components/Button';
-import Title from './components/Title';
+import TodoList from './components/TodoList';
 
 const App = () => {
-  const [age, setAge] = useState(23);
-  const [salary, setSalary] = useState(8000);
-
-  const incrementAge = useCallback(() => {
-    setAge(age + 1);
-  }, [age]);
-
-  const incrementSalary = useCallback(() => {
-    setSalary(salary + 1000);
-  }, [salary]);
-
   return (
     <>
       <div>
-        <Title />
-        <Count text='Age' count={age} />
-        <Count text='Salary' count={salary} />
-        <Button onClick={incrementAge}>Increment Age</Button>
-        <Button onClick={incrementSalary}>Increment Salary</Button>
+        <TodoList />
       </div>
     </>
   );
